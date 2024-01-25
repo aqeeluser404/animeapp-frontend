145b4a2079820a97087f4028e0762a90f461aa4c
aqeeluser404 <aqeelhanslo@gmail.com>
2024-01-25 20:45:52 +0200

    updated anime listing table style

--- src/components/AnimeList.vue
+++ src/components/AnimeList.vue
@@ -3,22 +3,22 @@
         <h1 class="text-center">Anime List</h1>
         <table class="table table-striped">
             <thead>
-                <th> Anime Id </th>
-                <th> Anime Title</th>
-                <th> Anime Genre </th>
-                <th> Anime Episode Count </th>
-                <th> Anime Status </th>
-                <th> Anime Rating </th>
+                <th class="table-head-text"> ID </th>
+                <th class="table-head-text"> TITLE </th>
+                <th class="table-head-text"> GENRE </th>
+                <th class="table-head-text"> EPISODE COUNT </th>
+                <th class="table-head-text"> STATUS </th>
+                <th class="table-head-text"> ANIME RATING </th>
             </thead>
             <tbody>
                 <!-- v-for iterates array of object variables -->
                 <tr v-for = "anime in animes" v-bind:key="anime.id">
-                    <td> {{ anime.id }} </td>
-                    <td> {{ anime.title }} </td>
-                    <td> {{ anime.genre }} </td>
-                    <td> {{ anime.episodeCount }} </td>
-                    <td> {{ anime.status }} </td>
-                    <td> {{ anime.rating }} </td>
+                    <td class="table-definition-text"> {{ anime.id }} </td>
+                    <td class="table-definition-text"> {{ anime.title }} </td>
+                    <td class="table-definition-text"> {{ anime.genre }} </td>
+                    <td class="table-definition-text"> {{ anime.episodeCount }} </td>
+                    <td class="table-definition-text"> {{ anime.status }} </td>
+                    <td class="table-definition-text"> {{ anime.rating }} </td>
                 </tr>
             </tbody>
         </table>
@@ -48,4 +48,21 @@ import AnimeService from '../services/AnimeService'
     }
 </script>
 
-<style scoped></style>
+<style scoped>
+.table-head-text {
+    font-size: 0.9rem;
+    text-align: left;
+    padding: 0.5rem;
+}
+.table-definition-text {
+    font-size: 0.8rem;
+    text-align: left;
+}
+.container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+</style>
