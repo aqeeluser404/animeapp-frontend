42143ef8e22c04c34d87abd2e5aaacd61dbfd00a
aqeeluser404 <aqeelhanslo@gmail.com>
2024-01-25 12:26:45 +0200

    Created service points for all crud operations

--- src/services/AnimeService.js
+++ src/services/AnimeService.js
@@ -1,16 +1,64 @@
 import axios from 'axios';
 
-const ANIME_API_BASE_URL = 'http://localhost:8080/api/anime/all';
+const ANIME_API_BASE_URL = 'http://localhost:8080/api/anime';
 
 class AnimeService {
     getAnimeList() {
-        return axios.get(ANIME_API_BASE_URL)
+        return axios.get(`${ANIME_API_BASE_URL}/all`)
             .then(response => {
-                console.log('Response:', response.data);
+                console.log('Anime List:', response.data);
                 return response;
             })
             .catch(error => {
-                console.error('Error:', error);
+                console.error('Error fetching anime list:', error);
+                throw error;
+            });
+    }
+
+    getAnimeById(animeId) {
+        return axios.get(`${ANIME_API_BASE_URL}/${animeId}`)
+            .then(response => {
+                console.log('Anime by ID:', response.data);
+                return response;
+            })
+            .catch(error => {
+                console.error('Error fetching anime by ID:', error);
+                throw error;
+            });
+    }
+
+    createAnime(animeData) {
+        return axios.post(`${ANIME_API_BASE_URL}/create`, animeData)
+            .then(response => {
+                console.log('Created Anime:', response.data);
+                return response;
+            })
+            .catch(error => {
+                console.error('Error creating anime:', error);
+                throw error;
+            });
+    }
+
+    updateAnime(animeId, animeData) {
+        return axios.put(`${ANIME_API_BASE_URL}/update/${animeId}`, animeData)
+            .then(response => {
+                console.log('Updated Anime:', response.data);
+                return response;
+            })
+            .catch(error => {
+                console.error('Error updating anime:', error);
+                throw error;
+            });
+    }
+
+    deleteAnime(animeId) {
+        return axios.delete(`${ANIME_API_BASE_URL}/delete/${animeId}`)
+            .then(response => {
+                console.log('Deleted Anime:', response.data);
+                return response;
+            })
+            .catch(error => {
+                console.error('Error deleting anime:', error);
                 throw error;
             });
     }
